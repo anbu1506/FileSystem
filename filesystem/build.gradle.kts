12623343a2a0e5f9f7139638b765644463ab8952
anbu1506 <arasu15062004@gmail.com>
2023-09-15 14:31:45 +0530

    library publish

--- filesystem/build.gradle.kts
+++ filesystem/build.gradle.kts
@@ -1,6 +1,21 @@
+buildscript {
+    extra["kotlin_version"] = "1.7.20"
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()            // << --- ADD This
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")          // << --- ADD This
 }
 
 android {
@@ -19,7 +34,8 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro" ,
+                "consumer-rules.pro"       // << --- ADD This
             )
         }
     }
@@ -32,11 +48,41 @@ android {
     }
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17) )       // << --- ADD This
+    }
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17            // << --- ADD This
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+
+// 5. Publishing:
+publishing {
+    publications {
+        create<MavenPublication>("maven"){
+            groupId = "com.github.anbu1506"
+            artifactId = "FileSystem"
+            version = "1.0"
+            pom {
+                description.set("FileSystem  for Android built with Sqlite and Kotlin")
+            }
+        }
+    }
+    repositories {               // << --- ADD This
+        mavenLocal()
+    }
+}
+
 dependencies {
 
 
     implementation ("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
