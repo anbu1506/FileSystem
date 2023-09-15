091de84efa7ec45c83329908092085c533b1fd66
anbu1506 <arasu15062004@gmail.com>
2023-09-15 11:35:25 +0530

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FileSystem"
+include(":app")
+include(":filesystem")
